bc32de9e62e89ab3617fb832e022e3e3943e27e0
miglvnc <mbogucki15@gmail.com>
2022-10-25 03:50:15 +0200

    finish functions, add style components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DogsListComponent } from './dogs-list/dogs-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DogDetailsComponent } from './dog-details/dog-details.component'
 
 import { FormsModule } from '@angular/forms'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DogsListComponent,
-    DogDetailsComponent
+    DogsListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/dogs-list/dogs-list.component.ts
+++ src/app/dogs-list/dogs-list.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Dog } from '../dog';
 
 @Component({
   selector: 'app-dogs-list',
@@ -11,20 +10,43 @@ export class DogsListComponent implements OnInit {
   
   dogListUrl = 'https://dog.ceo/api/breeds/list';
   dogList: any;
+  selectedValue: any;
+  dogImg: any;
+  link: any;
+  text: any;
+  header: any;
+  dogImgDisplay: any;
+  
+  
+  ChangeDog(e){
 
-  selectedDog?: Dog;
+    this.selectedValue = e.target.value;
+    this.header = "Nazwa: " + this.selectedValue
+    this.ImgChange(this.selectedValue)
+    this.wikiLink(this.selectedValue)
+    this.text = 'Więcej o piesku możesz przeczytać na Wikipedii: ';
 
+  }
+  
+  ImgChange(e){
+    this.http.get(`https://dog.ceo/api/breed/${e}/images/random`).subscribe((res) => {
+      this.dogImg = res;
+      this.dogImgDisplay = this.dogImg.message
+    })
+  }
+  
+  wikiLink(e){
+    this.link = `https://en.wikipedia.org/wiki/${e}`;
+    console.log(this.link)
+    return this.link;
+  }
+  
   constructor(private http : HttpClient) { }
-
+  
   ngOnInit(): void {
     this.http.get(this.dogListUrl).subscribe((res) => {
       this.dogList = res;
-      console.log(this.dogList)
+      
     })
   }
-
-  onSelect(dog: Dog): void {
-    this.selectedDog = dog;
-  }
-
 }
