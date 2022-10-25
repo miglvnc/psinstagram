aaffe145ba460765f5442c589659160d2d3db63e
miglvnc <mbogucki15@gmail.com>
2022-10-25 15:51:47 +0200

    git web pages deploy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -9,6 +9,9 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule,
         HttpClientTestingModule
+      ],
+      providers: [
+        HttpTestingController
       ],
       declarations: [
         AppComponent
