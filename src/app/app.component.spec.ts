cad7d96aefe28ac1decb9b20d6d8cb2b3b87f973
miglvnc <mbogucki15@gmail.com>
2022-10-25 04:04:52 +0200

    update style.sass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-psinstagram'`, () => {
+  it(`should have as title 'psinstagram'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-psinstagram');
+    expect(app.title).toEqual('psinstagram');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-psinstagram app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('psinstagram app is running!');
   });
 });
